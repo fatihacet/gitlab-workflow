16e8db6a67787da2d03b04a871fb7d0cf7d45f0d
Fatih Acet <acetfatih@gmail.com>
2018-01-26 19:06:51 +0100

    Implement fetchGitRemote with parsing git and https urls.

--- src/git_service.js
+++ src/git_service.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const execa = require('execa');
+const url = require('url');
 
 const getWorkspaceRootPath = () => {
   return vscode.workspace.rootPath;
@@ -25,5 +26,43 @@ async function fetchLastCommitId() {
   return await fetch(cmd);
 }
 
+async function fetchGitRemote() {
+  const url = await fetch('git ls-remote --get-url origin | cut -f 2 -d @ | cut -f 1 -d "/"');
+
+  if (url) {
+    const [schema, domain, namespace, project] = parseGitRemote(url);
+
+    return { schema, domain, namespace, project };
+  }
+
+  return null;
+}
+
+const parseGitRemote = (remote) => {
+  if (remote.startsWith('git@') || remote.startsWith('git://')) {
+    const match = new RegExp('^git(?:@|://)([^:/]+)(?::|:/|/)([^/]+)/(.+?)(?:.git)?$', 'i').exec(remote);
+
+    if (!match) {
+      return null;
+    }
+
+    return ['git:', ...match.slice(1, 4)];
+  } else {
+    const { protocol = 'https:', hostname, pathname } = url.parse(remote);
+
+    if (!hostname || !pathname) {
+      return null;
+    }
+
+    const match = pathname.match(/\/(.*?)\/(.*?)(?:.git)?$/);
+    if (!match) {
+      return null;
+    }
+
+    return [protocol, hostname, ...match.slice(1, 3)];
+  }
+}
+
 exports.fetchBranchName = fetchBranchName;
 exports.fetchLastCommitId = fetchLastCommitId;
+exports.fetchGitRemote = fetchGitRemote;
