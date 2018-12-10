39d74a2a39db7236a1dc6ecd3a694b2637200bb4
Fatih Acet <acetfatih@gmail.com>
2018-12-10 19:53:58 +0100

    SVG icon refactor.

--- src/webview/src/components/Discussion.vue
+++ src/webview/src/components/Discussion.vue
@@ -1,5 +1,6 @@
 <script>
 import Note from './Note';
+import icons from '../assets/icons';
 
 export default {
   name: 'Discussion',
@@ -31,16 +32,17 @@ export default {
       return this.isRepliesVisible ? 'Collapse replies' : 'Expand replies';
     },
     toggleRepliesIcon() {
-      const chevronDown = '<svg aria-hidden="true" data-prefix="fas" data-icon="chevron-down" class="svg-inline--fa fa-chevron-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>';
-      const chevronRight = '<svg aria-hidden="true" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg>';
-
-      return this.isRepliesVisible ? chevronDown : chevronRight;
+      return this.isRepliesVisible ? this.chevronDownSvg : this.chevronRightSvg;
     },
   },
   methods: {
     toggleReplies() {
       this.isRepliesVisible = !this.isRepliesVisible;
     },
+  },
+  created() {
+    this.chevronDownSvg = icons.chevronDown;
+    this.chevronRightSvg = icons.chevronRight;
   },
 };
 </script>
