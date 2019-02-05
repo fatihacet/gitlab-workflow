0c19857407ee3a43eacc5e1e1329519270d50552
Fatih Acet <acetfatih@gmail.com>
2019-02-05 22:22:54 +0100

    Only open issues inside VSCode

--- src/sidebar_tree_item.js
+++ src/sidebar_tree_item.js
@@ -5,9 +5,17 @@ class SidebarTreeItem extends vscode.TreeItem {
     super(title);
 
     if (data) {
+      let command = 'gl.showRichContent';
+      let arg = data;
+
+      if (data.sha) {
+        command = 'vscode.open';
+        arg = vscode.Uri.parse(data.web_url);
+      }
+
       this.command = {
-        command: 'gl.showRichContent',
-        arguments: [data],
+        command,
+        arguments: [arg],
       };
     }
   }
